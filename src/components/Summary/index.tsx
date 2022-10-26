0b76cc32df0891cb16902987043ad346ce805e09
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-10-26 17:57:29 -0300

    Componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+
+import { SummaryCard, SummaryContainer } from "./style";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>R$ 1.000,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+
+        <strong>R$ 1.259,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#ffffff" />
+        </header>
+
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+}
