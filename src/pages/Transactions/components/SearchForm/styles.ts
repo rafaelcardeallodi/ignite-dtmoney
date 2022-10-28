bf290d8e601bdf77fc8f4e9e8283b94f5e464a7e
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-10-28 19:23:00 -0300

    Aplicando React Hook Form

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -36,7 +36,12 @@ export const SearchFormContainer = styled.form`
 
     cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background-color: ${({ theme }) => theme["green-500"]};
       border-color: ${({ theme }) => theme["green-500"]};
       color: ${({ theme }) => theme["white"]};
